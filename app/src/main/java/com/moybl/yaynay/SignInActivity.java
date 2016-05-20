ddd34ac8ccd9955a40908007161e2dc8bbb4df85
paidgeek <tin.rabzelj@gmail.com>
2016-05-20 16:44:09 +0200

    app engine

--- app/src/main/java/com/moybl/yaynay/SignInActivity.java
+++ app/src/main/java/com/moybl/yaynay/SignInActivity.java
@@ -7,29 +7,24 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
-import android.widget.Toast;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
 	private static final String TAG = "SignInActivity";
 	private static final int RC_SIGN_IN = 9001;
 
 	private GoogleApiClient mGoogleApiClient;
-	private FirebaseAuth mFirebaseAuth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,78 +32,75 @@ public class SignInActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_sign_in);
 
 		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-				.requestIdToken(getString(R.string.default_web_client_id))
 				.requestEmail()
 				.build();
 
 		mGoogleApiClient = new GoogleApiClient.Builder(this)
-				.enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
-					@Override
-					public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-						Log.d(TAG, "connection failed");
-					}
-				})
+				.enableAutoManage(this, this)
 				.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
 				.build();
 
-		mFirebaseAuth = FirebaseAuth.getInstance();
-
-		SignInButton signInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+		SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
 		signInButton.setSize(SignInButton.SIZE_STANDARD);
 		signInButton.setScopes(gso.getScopeArray());
-		signInButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				signIn();
-			}
-		});
+
+		OptionalPendingResult<GoogleSignInResult> pendingResult =
+				Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+		if (pendingResult.isDone()) {
+			// There's immediate result available.
+			updateButtonsAndStatusFromSignInResult(pendingResult.get());
+		} else {
+			// There's no immediate result ready, displays some progress indicator and waits for the
+			// async callback.
+			//showProgressIndicator();
+			pendingResult.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+				@Override
+				public void onResult(@NonNull GoogleSignInResult result) {
+					updateButtonsAndStatusFromSignInResult(result);
+					//hideProgressIndicator();
+				}
+			});
+		}
+	}
+
+	private void updateButtonsAndStatusFromSignInResult(GoogleSignInResult result) {
+		if (result.isSuccess()) {
+			Intent intent = new Intent(this, HomeActivity.class);
+			startActivity(intent);
+		} else {
+			findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					signIn();
+				}
+			});
+		}
 	}
 
 	private void signIn() {
 		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-
 		startActivityForResult(signInIntent, RC_SIGN_IN);
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
+		// Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
 		if (requestCode == RC_SIGN_IN) {
 			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
 
-			if (result.isSuccess()) {
-				GoogleSignInAccount account = result.getSignInAccount();
+			Log.d(TAG, "handleSignInResult:" + result.isSuccess());
 
-				firebaseAuthWithGoogle(account);
-			} else {
-				Log.e(TAG, "Google Sign In failed.");
+			if (result.isSuccess()) {
+				Intent intent = new Intent(this, HomeActivity.class);
+				startActivity(intent);
 			}
 		}
 	}
 
-	private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-		Log.d(TAG, "firebaseAuthWithGooogle:" + acct.getId());
-
-		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-
-		mFirebaseAuth.signInWithCredential(credential)
-				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-						if (task.isSuccessful()) {
-							Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-							startActivity(intent);
-						} else {
-							Log.w(TAG, "signInWithCredential", task.getException());
-							Toast.makeText(SignInActivity.this, "Authentication failed.",
-									Toast.LENGTH_SHORT).show();
-
-						}
-					}
-				});
+	@Override
+	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 	}
 
 }
--- app/src/main/java/com/moybl/yaynay/YayNay.java
+++
@@ -1,7 +0,0 @@
-package com.moybl.yaynay;
-
-import android.app.Application;
-
-public class YayNay extends Application {
-
-}
