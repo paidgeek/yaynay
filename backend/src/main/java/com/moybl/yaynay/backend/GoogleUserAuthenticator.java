55d08d9eebec7e846a3daebac00b6118b346bdfb
paidgeek <tin.rabzelj@gmail.com>
2016-05-21 15:57:03 +0200

    auth fix

--- backend/src/main/java/com/moybl/yaynay/backend/GoogleUserAuthenticator.java
+++ backend/src/main/java/com/moybl/yaynay/backend/GoogleUserAuthenticator.java
@@ -8,16 +8,20 @@ import com.google.api.server.spi.auth.common.User;
 import com.google.api.server.spi.config.Authenticator;
 
 import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class GoogleUserAuthenticator implements Authenticator {
 
+	private static final Logger log = Logger.getLogger(GoogleUserAuthenticator.class.getName());
+
 	private GoogleIdTokenVerifier verifier;
 
 	public GoogleUserAuthenticator() {
 		verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
-				.setAudience(Collections.singletonList(Constants.ANDROID_CLIENT_ID))
+				.setAudience(Collections.singletonList(Constants.ANDROID_AUDIENCE))
 				.setIssuer("https://accounts.google.com")
 				.build();
 	}
@@ -28,7 +32,7 @@ public class GoogleUserAuthenticator implements Authenticator {
 
 		if (token != null) {
 			try {
-				GoogleIdToken idToken = verifier.verify(token);
+				GoogleIdToken idToken = verifier.verify(token.substring(token.indexOf(' ') + 1));
 				GoogleIdToken.Payload payload = idToken.getPayload();
 
 				String id = payload.getSubject();
