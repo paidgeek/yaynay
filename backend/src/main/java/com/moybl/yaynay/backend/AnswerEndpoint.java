0014969603d608b66861b9ee7a384a025abaa1ba
paidgeek <tin.rabzelj@gmail.com>
2016-05-24 05:05:11 +0200

    question conflicts

--- backend/src/main/java/com/moybl/yaynay/backend/AnswerEndpoint.java
+++ backend/src/main/java/com/moybl/yaynay/backend/AnswerEndpoint.java
@@ -3,11 +3,12 @@ package com.moybl.yaynay.backend;
 import com.google.api.server.spi.auth.common.User;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.response.ConflictException;
 import com.google.api.server.spi.response.NotFoundException;
 import com.google.api.server.spi.response.UnauthorizedException;
-import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 
+import com.googlecode.objectify.Key;
 import com.moybl.yaynay.backend.auth.AskerUser;
 import com.moybl.yaynay.backend.auth.YayNayAuthenticator;
 import com.moybl.yaynay.backend.model.Answer;
@@ -22,13 +23,27 @@ public class AnswerEndpoint extends YayNayEndpoint {
 			authenticators = YayNayAuthenticator.class
 	)
 	public void insert(User user, @Named("questionId") Long questionId, @Named("state") boolean state)
-			throws UnauthorizedException, NotFoundException {
+			throws UnauthorizedException, NotFoundException, ConflictException {
 		if (user == null) {
 			throw new UnauthorizedException("Unauthorized");
 		}
 
 		Asker asker = ((AskerUser) user).getAsker();
-		Key questionKey = KeyFactory.createKey("Question", questionId);
+
+		Key<Answer> answerKey = OfyService.ofy()
+				.load()
+				.type(Answer.class)
+				.filter("akserId", asker.getId())
+				.filter("questionId", questionId)
+				.keys()
+				.first()
+				.now();
+
+		if (answerKey != null) {
+			throw new ConflictException("Already answered");
+		}
+
+		Key questionKey = Key.create(Question.class, questionId);
 
 		Question question = OfyService.ofy()
 				.load()
